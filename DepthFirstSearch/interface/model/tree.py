0d8fc5669da716fd894ada9275c5d49abe1d730a
ktinsol <kostjaso96@gmail.com>
2024-08-16 20:14:58 +0200

    Changes to tree file

--- DepthFirstSearch/interface/model/tree.py
+++ DepthFirstSearch/interface/model/tree.py
@@ -1,4 +1,5 @@
 import random
+import uuid
 
 
 class Tree:
@@ -25,19 +26,30 @@ class Tree:
     def print_tree(self):
         if self.get_root_node() is None:
             print("No tree")
-        print(str(self.get_root_node().get_value()))
-        print(self.get_root_node())
-        node = self.get_root_node()
-        self.print_recursively(node)
+        stack = [self.get_root_node()]
+        self.print_recursively(len(stack), stack)
 
-    def print_recursively(self, node):
-        if node is None:
+    def print_recursively(self, size, stack):
+        if size <= 0:
             return
-        if node.get_child_nodes() is None:
-            return print(str(node.get_value()))
-        for child_node in node.get_child_nodes():
-            print(str(node.get_value()))
-            self.print_recursively(child_node)
+        output_string = ""
+        for i in range(size):
+            current_node = stack.pop(0)
+            parent_node_value = ""
+            if current_node is None:
+                continue
+            for parent_node in current_node.get_parent_nodes():
+                if parent_node is not None:
+                    parent_node_value += str(current_node.get_parent_nodes()[0].get_value())
+                else:
+                    parent_node_value = ""
+            output_string += " " + str(current_node.get_value()) + "(" + parent_node_value + ")"
+            if current_node.get_child_nodes() is None:
+                continue
+            for child in current_node.get_child_nodes():
+                stack.append(child)
+        print(output_string)
+        self.print_recursively(len(stack), stack)
 
 
 class Node:
@@ -46,6 +58,7 @@ class Node:
         self.__value = None
         self.__child_nodes = []
         self.__parent_nodes = []
+        self.__id = Node.generate_id()
 
     def add_child_node(self, node):
         self.__child_nodes.append(node)
@@ -57,7 +70,7 @@ class Node:
         return self.__child_nodes
 
     def get_parent_nodes(self):
-        return self.get_parent_nodes()
+        return self.__parent_nodes
 
     def set_value(self, value):
         self.__value = value
@@ -65,53 +78,43 @@ class Node:
     def get_value(self):
         return self.__value
 
-
-class TreeBuilder:
+    def get_id(self):
+        return self.__id
 
     @staticmethod
-    def build_tree(depth, width):
-        # Create tree object
-        tree = Tree()
-        # Create root node
-        root_node = TreeBuilder.create_node()
-        # Connect root node and tree object
-        tree.set_root_node(root_node)
-        # Create other nodes and add them to the tree
-        for i in range(depth-1):
-            for j in range(width):
-                node = TreeBuilder.create_node_randomly()
-                if node is not None:
-                    tree.add_node(node)
-        # Create child and parent relations
-        tree = TreeBuilder.create_parent_and_child_relations(tree)
-        return tree
+    def generate_id():
+        unique_id = uuid.uuid1()
+        return unique_id
+
+
+class TreeBuilder:
 
     @staticmethod
     def build_tree_recursively(depth, width):
         # Create tree object
         tree = Tree()
-        # Create root node
-        root_node = TreeBuilder.create_node()
         # Create tree recursively
-        tree.set_root_node(TreeBuilder.recursive_build_tree(0, depth, width, root_node))
-        # Build the tree based on the returned node with its children
+        tree.set_root_node(TreeBuilder.recursive_build_tree(0, depth, width, None))
+        # Build the tree array based on the returned node with its children
         tree.set_nodes(TreeBuilder.construct_tree(tree.get_root_node()))
         # Clean the tree of none values
         tree.set_nodes(TreeBuilder.remove_none_values_from_tree(tree.get_nodes()))
         # Return the tree object as output
         return tree
-        
+
     @staticmethod
     def recursive_build_tree(depth, limit, width, parent):
         if depth == limit:
             return None
         else:
+            child_node = None
             for i in range(width):
                 child_node = TreeBuilder.create_node_randomly(depth)
                 if child_node is None:
                     continue
                 child_node.add_parent_node(parent)
-                parent.add_child_node(child_node)
+                if parent is not None:
+                    parent.add_child_node(child_node)
                 # parent.add_child_node(TreeBuilder.recursive_build_tree(depth+1, limit, width, child_node))
                 child_node.add_child_node(TreeBuilder.recursive_build_tree(depth + 1, limit, width, child_node))
             return child_node
@@ -127,12 +130,8 @@ class TreeBuilder:
     @staticmethod
     def create_node():
         node = Node()
-        node.set_value(TreeBuilder.generate_random_value(0, 10))
+        node.set_value(random.randint(0, 10))
         return node
-
-    @staticmethod
-    def generate_random_value(min_limit, max_limit):
-        return random.randint(min_limit, max_limit)
 
     @staticmethod
     def create_parent_and_child_relations(tree):
